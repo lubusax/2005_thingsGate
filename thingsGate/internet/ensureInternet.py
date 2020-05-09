53ffe653ad9812b1af521df05e3a1e029bd192cc
lubusax <lu.bu.sax@gmail.com>
2020-05-09 17:05:39 +0200

    autosave

--- thingsGate/internet/ensureInternet.py
+++ thingsGate/internet/ensureInternet.py
@@ -1,4 +1,39 @@
-import time
+import time,socket,fcntl,struct
+
+def check_connection():
+  ifaces = ['eth0','wlan0']
+  connected = []
+
+  i = 0
+  for ifname in ifaces:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+      socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+        )[20:24])
+      connected.append(ifname)
+      print ("%s is connected" % ifname)
+    except:
+      print ("%s is not connected" % ifname)
+  i += 1
+
+  return connected
+
+
+
+def isAnyInterfaceConnected():
+  result=False
+  connected_ifaces = check_connection()
+  if len(connected_ifaces) == 0:
+      print ('not connected to any network')
+  else:
+      result= True
+      print ('connected to a network using the following interface(s):')
+      for x in connected_ifaces:
+          print ('\t%s' % x)
+  return result
 
 def ensureInternet( semaphoreInternet,
                     semaphoreEndInternet):
@@ -11,9 +46,13 @@ def ensureInternet( semaphoreInternet,
     semaphoreEndInternet.release()
     # release the semaphore inmediately
     # to allow the acquire somewhere else 
-    # if this process needs to get a stop signal to end 
+    # if this process needs to get a stop signal to end
+    if not isAnyInterfaceConnected(): 
+      print ('not connected to any network')
+    else:
+      print ('there is at least one network connection active')
 
-    time.sleep(0.1) #do some stuff
+    time.sleep(0.7) #do some stuff
 
   # here you can make some arrangements before
   # closing the process
